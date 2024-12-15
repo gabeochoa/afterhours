9b86b2ffeb326147be71bab38aa8156751de1115
Gabe Ochoa <gabeochoa@meta.com>
2024-12-15 12:27:40 -0500

    add render and const functions

--- src/system.h
+++ src/system.h
@@ -12,36 +12,80 @@ class SystemBase {
 public:
   SystemBase() {}
   virtual ~SystemBase() {}
-  virtual void for_each(Entity &) = 0;
+  virtual void for_each(Entity &, float) = 0;
+  virtual void for_each(const Entity &, float) const = 0;
 };
 
 template <typename... Components> struct System : SystemBase {
 
-  void for_each(Entity &entity) {
+  void for_each(Entity &entity, float dt) {
     if constexpr (sizeof...(Components) > 0) {
       if ((entity.template has<Components>() && ...)) {
-        for_each_with(entity, entity.template get<Components>()...);
+        for_each_with(entity, entity.template get<Components>()..., dt);
       }
     } else {
-      for_each_with(entity);
+      for_each_with(entity, dt);
     }
   }
 
-  virtual void for_each_with(Entity &, Components &...) = 0;
+  virtual void for_each_with(Entity &, Components &..., float) {}
+
+  void for_each(const Entity &entity, float dt) const {
+    if constexpr (sizeof...(Components) > 0) {
+      if ((entity.template has<Components>() && ...)) {
+        for_each_with(entity, entity.template get<Components>()..., dt);
+      }
+    } else {
+      for_each_with(entity, dt);
+    }
+  }
+  virtual void for_each_with(const Entity &, const Components &...,
+                             float) const {}
 };
 
-struct SystemManager {
-  std::vector<std::unique_ptr<SystemBase>> systems_;
+#include "entity_helper.h"
 
-  void register_system(std::unique_ptr<SystemBase> system) {
-    systems_.emplace_back(std::move(system));
+struct SystemManager {
+  std::vector<std::unique_ptr<SystemBase>> update_systems_;
+  std::vector<std::unique_ptr<SystemBase>> render_systems_;
+
+  void register_update_system(std::unique_ptr<SystemBase> system) {
+    update_systems_.emplace_back(std::move(system));
   }
 
-  void tick(Entities &entities) {
-    for (auto &system : systems_) {
+  void register_render_system(std::unique_ptr<SystemBase> system) {
+    render_systems_.emplace_back(std::move(system));
+  }
+
+  void tick(Entities &entities, float dt) {
+    for (auto &system : update_systems_) {
       for (std::shared_ptr<Entity> entity : entities) {
-        system->for_each(*entity);
+        system->for_each(*entity, dt);
       }
     }
+  }
+
+  void render(const Entities &entities, float dt) {
+    for (const auto &system : render_systems_) {
+      for (std::shared_ptr<Entity> entity : entities) {
+        const Entity &e = *entity;
+        system->for_each(e, dt);
+      }
+    }
+  }
+
+  void tick_all(float dt) {
+    auto &entities = EntityHelper::get_entities_for_mod();
+    tick(entities, dt);
+  }
+
+  void render_all(float dt) {
+    const auto &entities = EntityHelper::get_entities();
+    render(entities, dt);
+  }
+
+  void run(float dt) {
+    tick_all(dt);
+    render_all(dt);
   }
 };
