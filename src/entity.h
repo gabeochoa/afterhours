742c6336adc040052ad7ac25b986a854a3925ce2
Gabe Ochoa <gabeochoa@meta.com>
2024-12-19 01:41:17 -0500

    update define names for logging

--- src/entity.h
+++ src/entity.h
@@ -14,14 +14,16 @@ template <typename Base, typename Derived> bool child_of(Derived *derived) {
   return dynamic_cast<Base *>(derived) != nullptr;
 }
 
-#if !defined(log_info)
+#if !defined(AFTER_HOURS_REPLACE_LOGGING)
 // TODO eventually implement these
+// TODO move to a log.h file and include them in the other parts of the library
 inline void log_trace(...) {}
 inline void log_info(...) {}
 inline void log_warn(...) {}
 inline void log_error(...) {}
 #endif
-#if !defined(VALIDATE)
+
+#if !defined(AFTER_HOURS_REPLACE_VALIDATE)
 inline void VALIDATE(...) {}
 #endif
 
