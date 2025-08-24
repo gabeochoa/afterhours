80791cefa22bbf041237386edfb2a1c589ea0b34
Gabe Ochoa <gabeochoa@meta.com>
2025-08-24 02:09:08 -0400

    derived children update

--- src/plugins/ui/systems.h
+++ src/plugins/ui/systems.h
@@ -34,13 +34,14 @@ struct BeginUIContextManager : System<UIContext<InputAction>> {
 
   // TODO this should live inside input_system
   // but then it would require magic_enum as a dependency
-  InputBits inputs_as_bits(
-      const std::vector<input::ActionDone> &inputs) const {
+  InputBits inputs_as_bits(const std::vector<input::ActionDone> &inputs) const {
     InputBits output;
     for (auto &input : inputs) {
       if (input.amount_pressed <= 0.f)
         continue;
-      output[magic_enum::enum_index<InputAction>(static_cast<InputAction>(input.action)).value()] = true;
+      output[magic_enum::enum_index<InputAction>(
+                 static_cast<InputAction>(input.action))
+                 .value()] = true;
     }
     return output;
   }
@@ -51,8 +52,7 @@ struct BeginUIContextManager : System<UIContext<InputAction>> {
     context.mouseLeftDown = input::is_mouse_button_down(0);
 
     {
-      input::PossibleInputCollector inpc =
-          input::get_input_collector();
+      input::PossibleInputCollector inpc = input::get_input_collector();
       if (inpc.has_value()) {
         context.all_actions = inputs_as_bits(inpc.inputs());
         for (auto &actions_done : inpc.inputs_pressed()) {
@@ -216,20 +216,18 @@ struct SystemWithUIContext : System<UIComponent, Components...> {};
 
 template <typename InputAction>
 struct HandleClicks : SystemWithUIContext<ui::HasClickListener> {
-
   UIContext<InputAction> *context;
 
   virtual void once(float) override {
     this->context =
         EntityHelper::get_singleton_cmp<ui::UIContext<InputAction>>();
-    this->include_derived_children = true;
+    // this->include_derived_children = true;
   }
 
   virtual ~HandleClicks() {}
 
-  virtual void for_each_with_derived(Entity &entity, UIComponent &component,
-                                     HasClickListener &hasClickListener,
-                                     float) {
+  virtual void for_each_with(Entity &entity, UIComponent &component,
+                             HasClickListener &hasClickListener, float) {
     hasClickListener.down = false;
 
     if (!component.was_rendered_to_screen)
@@ -248,6 +246,53 @@ struct HandleClicks : SystemWithUIContext<ui::HasClickListener> {
       context->set_focus(entity.id);
       hasClickListener.cb(entity);
       hasClickListener.down = true;
+    }
+
+    process_derived_children(entity, component);
+  }
+
+private:
+  void process_derived_children(Entity &parent, UIComponent &parent_component) {
+    if (!parent.has<UIComponent>()) {
+      return;
+    }
+
+    auto &ui_component = parent.get<UIComponent>();
+    for (auto child_id : ui_component.children) {
+      auto child_entity = EntityHelper::getEntityForID(child_id);
+      if (!child_entity.has_value()) {
+        continue;
+      }
+
+      Entity &child = child_entity.asE();
+      if (!child.has<UIComponent>() || !child.has<ui::HasClickListener>()) {
+        continue;
+      }
+
+      auto &child_component = child.get<UIComponent>();
+      auto &child_hasClickListener = child.get<ui::HasClickListener>();
+
+      child_hasClickListener.down = false;
+
+      if (!child_component.was_rendered_to_screen)
+        continue;
+
+      context->active_if_mouse_inside(child.id, child_component.rect());
+
+      if (context->has_focus(child.id) &&
+          context->pressed(InputAction::WidgetPress)) {
+        context->set_focus(child.id);
+        child_hasClickListener.cb(child);
+        child_hasClickListener.down = true;
+      }
+
+      if (context->is_mouse_click(child.id)) {
+        context->set_focus(child.id);
+        child_hasClickListener.cb(child);
+        child_hasClickListener.down = true;
+      }
+
+      process_derived_children(child, child_component);
     }
   }
 };
@@ -315,12 +360,12 @@ struct HandleLeftRight : SystemWithUIContext<ui::HasLeftRightListener> {
   virtual void once(float) override {
     this->context =
         EntityHelper::get_singleton_cmp<ui::UIContext<InputAction>>();
-    this->include_derived_children = true;
+    // this->include_derived_children = true;
   }
   virtual ~HandleLeftRight() {}
 
-  virtual void for_each_with_derived(Entity &entity, UIComponent &component,
-                                     HasLeftRightListener &listener, float) {
+  virtual void for_each_with(Entity &entity, UIComponent &component,
+                             HasLeftRightListener &listener, float) {
     if (!component.was_rendered_to_screen)
       return;
 
@@ -336,6 +381,49 @@ struct HandleLeftRight : SystemWithUIContext<ui::HasLeftRightListener> {
         context->is_held_down(InputAction::WidgetRight)) {
       listener.cb(entity, +1);
     }
+
+    process_derived_children(entity, component);
+  }
+
+private:
+  void process_derived_children(Entity &parent, UIComponent &parent_component) {
+    if (!parent.has<UIComponent>()) {
+      return;
+    }
+
+    auto &ui_component = parent.get<UIComponent>();
+    for (auto child_id : ui_component.children) {
+      auto child_entity = EntityHelper::getEntityForID(child_id);
+      if (!child_entity.has_value()) {
+        continue;
+      }
+
+      Entity &child = child_entity.asE();
+      if (!child.has<UIComponent>() || !child.has<ui::HasLeftRightListener>()) {
+        continue;
+      }
+
+      auto &child_component = child.get<UIComponent>();
+      auto &child_listener = child.get<ui::HasLeftRightListener>();
+
+      if (!child_component.was_rendered_to_screen)
+        continue;
+
+      if (!context->has_focus(child.id))
+        continue;
+
+      // TODO consider using a different repeat rate
+      if (context->pressed(InputAction::WidgetLeft) ||
+          context->is_held_down(InputAction::WidgetLeft)) {
+        child_listener.cb(child, -1);
+      }
+      if (context->pressed(InputAction::WidgetRight) ||
+          context->is_held_down(InputAction::WidgetRight)) {
+        child_listener.cb(child, +1);
+      }
+
+      process_derived_children(child, child_component);
+    }
   }
 };
 
@@ -348,15 +436,14 @@ struct HandleSelectOnFocus
   virtual void once(float) override {
     this->context =
         EntityHelper::get_singleton_cmp<ui::UIContext<InputAction>>();
-    this->include_derived_children = true;
+    // this->include_derived_children = true;
   }
 
   virtual ~HandleSelectOnFocus() {}
 
-  virtual void for_each_with_derived(Entity &entity, UIComponent &component,
-                                     SelectOnFocus &selectOnFocus,
-                                     HasClickListener &hasClickListener,
-                                     float) {
+  virtual void for_each_with(Entity &entity, UIComponent &component,
+                             SelectOnFocus &selectOnFocus,
+                             HasClickListener &hasClickListener, float) {
     if (!component.was_rendered_to_screen)
       return;
 
@@ -376,6 +463,55 @@ struct HandleSelectOnFocus
     } else {
       last_focused_ids.erase(entity.id);
     }
+
+    process_derived_children(entity, component);
+  }
+
+private:
+  void process_derived_children(Entity &parent, UIComponent &parent_component) {
+    if (!parent.has<UIComponent>()) {
+      return;
+    }
+
+    auto &ui_component = parent.get<UIComponent>();
+    for (auto child_id : ui_component.children) {
+      auto child_entity = EntityHelper::getEntityForID(child_id);
+      if (!child_entity.has_value()) {
+        continue;
+      }
+
+      Entity &child = child_entity.asE();
+      if (!child.has<UIComponent>() || !child.has<ui::SelectOnFocus>() ||
+          !child.has<ui::HasClickListener>()) {
+        continue;
+      }
+
+      auto &child_component = child.get<UIComponent>();
+      auto &child_selectOnFocus = child.get<ui::SelectOnFocus>();
+      auto &child_hasClickListener = child.get<ui::HasClickListener>();
+
+      if (!child_component.was_rendered_to_screen)
+        continue;
+
+      // Check if this entity just gained focus
+      bool currently_focused = context->has_focus(child.id);
+      bool was_focused = last_focused_ids.contains(child.id);
+
+      if (currently_focused && !was_focused) {
+        // Component just gained focus, trigger the click
+        child_hasClickListener.cb(child);
+        child_hasClickListener.down = true;
+      }
+
+      // Update our tracking
+      if (currently_focused) {
+        last_focused_ids.insert(child.id);
+      } else {
+        last_focused_ids.erase(child.id);
+      }
+
+      process_derived_children(child, child_component);
+    }
   }
 };
 
@@ -392,98 +528,225 @@ struct UpdateDropdownOptions
 
   UpdateDropdownOptions()
       : SystemWithUIContext<HasDropdownState, HasChildrenComponent>() {
-    // TODO figure out if this actually will cause trouble 
-    // Remove include_derived_children since we want to process entities with direct components
-    #if __WIN32 
-    // this->include_derived_children = true;
-    #else
-    this->include_derived_children = true;
-    #endif
+// TODO figure out if this actually will cause trouble
+// Remove include_derived_children since we want to process entities with direct
+// components
+#if __WIN32
+// this->include_derived_children = true;
+#else
+// this->include_derived_children = true;
+#endif
   }
 
-  #if __WIN32 
+#if __WIN32
   virtual void for_each_with(Entity &entity, UIComponent &component,
-                                     HasDropdownState &hasDropdownState,
-                                     HasChildrenComponent &hasChildren, float) {
+                             HasDropdownState &hasDropdownState,
+                             HasChildrenComponent &hasChildren, float){
 
-  #else
-  virtual void for_each_with_derived(Entity &entity, UIComponent &component,
-                                     HasDropdownState &hasDropdownState,
-                                     HasChildrenComponent &hasChildren, float) {
-  #endif
-    // The system should automatically filter entities to only those with required components
-    // No need to manually check since SystemWithUIContext<HasDropdownState, HasChildrenComponent> handles this
-    
-    auto options = hasDropdownState.options;
-    hasDropdownState.options = hasDropdownState.fetch_options(hasDropdownState);
-    
-    // Validate returned options
-    // TODO replace with a pound define
-    if (hasDropdownState.options.size() > 100) {
-      log_error("UpdateDropdownOptions: Entity {} - fetch_options returned {} options - ABORTING", 
-                entity.id, hasDropdownState.options.size());
+#else
+  virtual void for_each_with(Entity &entity, UIComponent &component,
+                             HasDropdownState &hasDropdownState,
+                             HasChildrenComponent &hasChildren, float) {
+#endif
+      // The system should automatically filter entities to only those with
+      // required components No need to manually check since
+      // SystemWithUIContext<HasDropdownState, HasChildrenComponent> handles
+      // this
+
+      auto options = hasDropdownState.options;
+  hasDropdownState.options = hasDropdownState.fetch_options(hasDropdownState);
+
+  // Validate returned options
+  // TODO replace with a pound define
+  if (hasDropdownState.options.size() > 100) {
+    log_error("UpdateDropdownOptions: Entity {} - fetch_options returned {} "
+              "options - ABORTING",
+              entity.id, hasDropdownState.options.size());
+    return;
+  }
+
+  // detect if the options changed or if the state changed
+  {
+    bool changed = false;
+    if (options.size() != hasDropdownState.options.size()) {
+      changed = true;
+    }
+
+    // Validate the order and which strings have changed
+    for (size_t i = 0; i < options.size(); ++i) {
+      if (i >= hasDropdownState.options.size() ||
+          options[i] != hasDropdownState.options[i]) {
+        changed = true;
+      }
+    }
+
+    // Check for new options
+    if (hasDropdownState.options.size() > options.size()) {
+      for (size_t i = options.size(); i < hasDropdownState.options.size();
+           ++i) {
+        changed = true;
+      }
+    }
+
+    if (hasDropdownState.changed_since) {
+      changed = true;
+      hasDropdownState.changed_since = false;
+    }
+
+    if (!changed) {
       return;
+    }
+  }
+
+  options = hasDropdownState.options;
+  component.children.clear();
+
+  if (hasChildren.children.size() == 0) {
+    // no children and no options :)
+    if (hasDropdownState.options.size() == 0) {
+      log_warn("You have a dropdown with no options");
+      return;
+    }
+
+    for (size_t i = 0; i < options.size(); i++) {
+      Entity &child = EntityHelper::createEntity();
+      child.addComponent<UIComponentDebug>("dropdown_option");
+      child.addComponent<UIComponent>(child.id)
+          .set_desired_width(ui::Size{
+              .dim = ui::Dim::Percent,
+              .value = 1.f,
+          })
+          .set_desired_height(ui::Size{
+              .dim = ui::Dim::Pixels,
+              .value = component.desired[Axis::Y].value,
+          })
+          .set_parent(entity.id);
+      child.addComponent<ui::HasLabel>(options[i], false);
+      child.addComponent<ui::HasClickListener>([i, &entity](Entity &) {
+        ui::HasDropdownState &hds = entity.get_with_child<HasDropdownState>();
+        hds.changed_since = true;
+        hds.on = !hds.on;
+
+        hds.last_option_clicked = i;
+        if (hds.on_option_changed)
+          hds.on_option_changed(i);
+      });
+      hasChildren.add_child(child);
+    }
+  }
+  // If we get here, we should have num_options children...
+
+  if (!hasDropdownState.on) {
+    // just draw the selected one...
+    EntityID child_id =
+        hasChildren.children[hasDropdownState.last_option_clicked];
+    component.add_child(child_id);
+  } else {
+    for (EntityID child_id : hasChildren.children) {
+      component.add_child(child_id);
+    }
+  }
+
+  process_derived_children(entity, component, hasDropdownState, hasChildren);
+}
+
+private
+    : void
+      process_derived_children(Entity &parent, UIComponent &parent_component,
+                               HasDropdownState &parent_hasDropdownState,
+                               HasChildrenComponent &parent_hasChildren) {
+  if (!parent.has<UIComponent>()) {
+    return;
+  }
+
+  auto &ui_component = parent.get<UIComponent>();
+  for (auto child_id : ui_component.children) {
+    auto child_entity = EntityHelper::getEntityForID(child_id);
+    if (!child_entity.has_value()) {
+      continue;
+    }
+
+    Entity &child = child_entity.asE();
+    if (!child.has<UIComponent>() || !child.has<HasDropdownState>() ||
+        !child.has<HasChildrenComponent>()) {
+      continue;
+    }
+
+    auto &child_component = child.get<UIComponent>();
+    auto &child_hasDropdownState = child.get<HasDropdownState>();
+    auto &child_hasChildren = child.get<HasChildrenComponent>();
+
+    auto options = child_hasDropdownState.options;
+    child_hasDropdownState.options =
+        child_hasDropdownState.fetch_options(child_hasDropdownState);
+
+    // Validate returned options
+    if (child_hasDropdownState.options.size() > 100) {
+      log_error("UpdateDropdownOptions: Entity {} - fetch_options returned {} "
+                "options - ABORTING",
+                child.id, child_hasDropdownState.options.size());
+      continue;
     }
 
     // detect if the options changed or if the state changed
     {
       bool changed = false;
-      if (options.size() != hasDropdownState.options.size()) {
+      if (options.size() != child_hasDropdownState.options.size()) {
         changed = true;
       }
 
       // Validate the order and which strings have changed
       for (size_t i = 0; i < options.size(); ++i) {
-        if (i >= hasDropdownState.options.size() ||
-            options[i] != hasDropdownState.options[i]) {
+        if (i >= child_hasDropdownState.options.size() ||
+            options[i] != child_hasDropdownState.options[i]) {
           changed = true;
         }
       }
 
       // Check for new options
-      if (hasDropdownState.options.size() > options.size()) {
-        for (size_t i = options.size(); i < hasDropdownState.options.size();
-             ++i) {
+      if (child_hasDropdownState.options.size() > options.size()) {
+        for (size_t i = options.size();
+             i < child_hasDropdownState.options.size(); ++i) {
           changed = true;
         }
       }
 
-      if (hasDropdownState.changed_since) {
+      if (child_hasDropdownState.changed_since) {
         changed = true;
-        hasDropdownState.changed_since = false;
+        child_hasDropdownState.changed_since = false;
       }
 
       if (!changed) {
-        return;
+        continue;
       }
     }
 
-    options = hasDropdownState.options;
-    component.children.clear();
+    options = child_hasDropdownState.options;
+    child_component.children.clear();
 
-    if (hasChildren.children.size() == 0) {
+    if (child_hasChildren.children.size() == 0) {
       // no children and no options :)
-      if (hasDropdownState.options.size() == 0) {
+      if (child_hasDropdownState.options.size() == 0) {
         log_warn("You have a dropdown with no options");
-        return;
+        continue;
       }
-      
+
       for (size_t i = 0; i < options.size(); i++) {
-        Entity &child = EntityHelper::createEntity();
-        child.addComponent<UIComponentDebug>("dropdown_option");
-        child.addComponent<UIComponent>(child.id)
+        Entity &grandchild = EntityHelper::createEntity();
+        grandchild.addComponent<UIComponentDebug>("dropdown_option");
+        grandchild.addComponent<UIComponent>(grandchild.id)
             .set_desired_width(ui::Size{
                 .dim = ui::Dim::Percent,
                 .value = 1.f,
             })
             .set_desired_height(ui::Size{
                 .dim = ui::Dim::Pixels,
-                .value = component.desired[Axis::Y].value,
+                .value = child_component.desired[Axis::Y].value,
             })
-            .set_parent(entity.id);
-        child.addComponent<ui::HasLabel>(options[i], false);
-        child.addComponent<ui::HasClickListener>([i, &entity](Entity &) {
-          ui::HasDropdownState &hds = entity.get_with_child<HasDropdownState>();
+            .set_parent(child.id);
+        grandchild.addComponent<ui::HasLabel>(options[i], false);
+        grandchild.addComponent<ui::HasClickListener>([i, &child](Entity &) {
+          ui::HasDropdownState &hds = child.get_with_child<HasDropdownState>();
           hds.changed_since = true;
           hds.on = !hds.on;
 
@@ -491,24 +754,29 @@ struct UpdateDropdownOptions
           if (hds.on_option_changed)
             hds.on_option_changed(i);
         });
-        hasChildren.add_child(child);
+        child_hasChildren.add_child(grandchild);
       }
     }
     // If we get here, we should have num_options children...
 
-    if (!hasDropdownState.on) {
+    if (!child_hasDropdownState.on) {
       // just draw the selected one...
-      EntityID child_id =
-          hasChildren.children[hasDropdownState.last_option_clicked];
-      component.add_child(child_id);
+      EntityID grandchild_id =
+          child_hasChildren
+              .children[child_hasDropdownState.last_option_clicked];
+      child_component.add_child(grandchild_id);
     } else {
-      for (EntityID child_id : hasChildren.children) {
-        component.add_child(child_id);
+      for (EntityID grandchild_id : child_hasChildren.children) {
+        child_component.add_child(grandchild_id);
       }
     }
-  }
-};
 
-} // namespace ui
+    process_derived_children(child, child_component, child_hasDropdownState,
+                             child_hasChildren);
+  }
+}
+}; // namespace ui
+
+} // namespace afterhours
 
 } // namespace afterhours
